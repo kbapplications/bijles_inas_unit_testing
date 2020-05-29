d2ec24f25983afd05a156c18ac270b3024edeee3
Kevin Beye <kbeye1999@hotmail.com>
2020-05-29 16:05:16 +0200

    Update #1

---
+++ src/main/java/bijles/inas/test/App.java
@@ -0,0 +1,13 @@
+package bijles.inas.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/bijles/inas/test/models/UserTest.java
@@ -0,0 +1,11 @@
+package bijles.inas.test.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTest {
+    @Test
+    public void userTestSuccess() {
+        Assert.assertTrue(true);
+    }
+}
